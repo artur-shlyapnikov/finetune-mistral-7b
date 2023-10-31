f4ef30d5cf9c73e7ce4d41bdc13d7808dc517066
Artur Shlyapnikov <a.shlyapnikov@truvity.com>
2023-10-31 13:29:54 +0200

    - chore(install-deps.sh): add shebang and set -e to ensure script execution stops on error - feat(install-deps.sh): add command to launch Axolotl training using accelerate - feat(mistral-qlora.yml): add ds_type field to specify dataset type as json

--- install-deps.sh
+++ install-deps.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 set -e
 
 git clone https://github.com/OpenAccess-AI-Collective/axolotl
 cd axolotl
-
 pip3 install packaging
 pip3 install -e '.[flash-attn,deepspeed]'
 pip3 install -U git+https://github.com/huggingface/peft.git
+accelerate launch -m axolotl.cli.train ../mistral-qlora.yml
